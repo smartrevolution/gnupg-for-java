52f19f7ed9eb10eeb61850a2b1d467c7c23bfc97
Hans-Christoph Steiner <hans@eds.org>
2013-07-19 19:05:30 -0400

    reduce the keylist return to the bare minimum for speed
    This makes the keylist functions return much less information, so hopefully
    it will speed things up.  There should be an option for getting the full
    info when its needed.
    
    refs #1621

--- jni/GnuPGContext.c
+++ jni/GnuPGContext.c
@@ -134,10 +134,7 @@ Java_com_freiheit_gnupg_GnuPGContext_gpgmeNew(JNIEnv * env, jobject self)
     gpgme_new(&ctx);
     gpgme_set_armor(ctx, 1);
     gpgme_set_textmode(ctx, 1);
-    gpgme_set_keylist_mode(ctx,
-                           GPGME_KEYLIST_MODE_LOCAL \
-                           | GPGME_KEYLIST_MODE_SIGS \
-                           | GPGME_KEYLIST_MODE_SIG_NOTATIONS);
+    gpgme_set_keylist_mode(ctx, GPGME_KEYLIST_MODE_LOCAL);
 
     return LNG(ctx);
 }
