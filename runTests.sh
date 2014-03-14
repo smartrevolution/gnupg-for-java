09bc751411fac9300b9d34727cccc82b335b5c60
Hans-Christoph Steiner <hans@eds.org>
2014-03-13 20:12:36 -0400

    add jar path to runTests.sh

--- runTests.sh
+++ runTests.sh
@@ -1,4 +1,4 @@
 #STYLE=textui
 STYLE=swingui
 #STYLE=awtui
-java -Djava.library.path=`pwd`/jni -cp build/jar/gnupg-for-java-0.1.6.jar:lib/junit-3.8.1.jar junit.${STYLE}.TestRunner com.freiheit.gnupg.tests.GnuPGTestSuite
+java -Djava.library.path=`pwd`/jni:`pwd`/lib -cp build/jar/gnupg-for-java-0.1.6.jar:lib/junit-3.8.1.jar junit.${STYLE}.TestRunner com.freiheit.gnupg.tests.GnuPGTestSuite
