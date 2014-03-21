9e54a1746953db343b56943b8bc71fa677901936
Hans-Christoph Steiner <hans@eds.org>
2014-03-21 16:17:19 -0400

    update format-code.sh to get closest to current style
    Running the original script made a lot of changes to the code, the edits to
    the script caused the least amount of changes for me.

--- jni/format-code.sh
+++ jni/format-code.sh
@@ -6,12 +6,7 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
-$ASTYLE --help 2>&1 | grep "pad-first-paren-out" > /dev/null
-if [[ $? -eq 0 ]]; then
-    ASTYLE_ARGS="--style=kr --indent=spaces=4 --convert-tabs --pad-oper --unpad-paren --pad-first-paren-out --pad-header --align-pointer=type --align-reference=type"
-else
-    ASTYLE_ARGS="--style=kr --indent=spaces=4 --convert-tabs --pad-oper  --pad-header --align-pointer=type --align-reference=type"
-fi
+ASTYLE_ARGS="--style=kr --indent=spaces=4 --convert-tabs --pad-oper --unpad-paren --pad-header --align-pointer=type --align-reference=type"
 
 $ASTYLE $ASTYLE_ARGS *.h
 $ASTYLE $ASTYLE_ARGS *.c
